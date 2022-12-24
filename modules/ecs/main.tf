987505a78df4ac24e3e2cfbcbf42280ca6dfea8f
Chimezirim Ugochukwu <L00170994@atu.ie>
2022-12-24 12:03:33 +0000

    modified container  imager uri

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -71,7 +71,7 @@ resource "aws_ecs_service" "ecs_service" {
   cluster           = aws_ecs_cluster.ecs_cluster.id
   task_definition   = aws_ecs_task_definition.ecs_task_definition.arn
   platform_version  = "LATEST"
-  desired_count     = 1
+  desired_count     = 3
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
 
